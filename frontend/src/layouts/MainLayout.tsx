2ee9708fa2839bc6dbaf160ec31529fbc6e6a964
vanshika2818 <saxenavanshika18mar@gmail.com>
2025-08-15 16:53:04 +0530

    done

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,7 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
@@ -9,7 +6,6 @@ const MainLayout = () => {
       <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 };
